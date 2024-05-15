3971fff0d1d7bf09f9b1a25345a85c5ce8a9efe5
74Jesse19 <jesse_1974@att.net>
2024-05-15 14:25:43 -0500

    fixing mistake

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,81 @@ resource "aws_subnet" "public" {
 
   tags = {
     Name = "Public Subnet"
+  }
+}
+
+# Create route table for public subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+# Create route to internet gateway
+resource "aws_route" "public_route" {
+  route_table_id  = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id      = aws_internet_gateway.gateway.id
+}
+
+# Associate public subnet with route table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id  = aws_route_table.public.id
+}
+
+# Create security group to allow SSH access
+resource "aws_security_group" "ssh" {
+  name = "SSH Access"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch 2 EC2 Instances
+resource "aws_instance" "webserver" {
+  count         = 2
+  ami           = "ami-0bb84b8ffd87024d8" # Update with your desired AMI
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "Web Server ${count.index}"
+  }
+}
+
+# Create DynamoDB table named workouts and add
+resource "aws_dynamodb_table" "workouts" {
+  name = "workouts"
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+ #throughput settings
+  read_capacity = 5
+  write_capacity = 5
+}
+
+# Output public IP addresses of the instances
+output "public_ip" {
+  value = aws_instance.webserver[*].public_ip
+}
