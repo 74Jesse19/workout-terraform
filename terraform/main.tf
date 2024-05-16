dc1b7a8dbc804470dd2583b0d8f7581f2857adb2
74Jesse19 <jesse_1974@att.net>
2024-05-16 16:36:38 -0500

    updated main.tf to complete the lab infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,27 @@ resource "aws_route_table_association" "public" {
   route_table_id  = aws_route_table.public.id
 }
 
-# Create security group to allow SSH access
+# Create security group to allow http access
+resource "aws_security_group" "http" {
+  name = "http Access"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_security_group" "ssh" {
   name = "SSH Access"
   vpc_id = aws_vpc.main.id
@@ -68,21 +88,70 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_iam_role" "backend_dynamodb_access" {
+  name = "backend-dynamodb-access"
 
-# Launch 2 EC2 Instances
-resource "aws_instance" "webserver" {
-  count         = 2
-  ami           = "ami-0bb84b8ffd87024d8" 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "backend_dynamodb_policy" {
+  role       = aws_iam_role.backend_dynamodb_access.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_instance" "artifactory" {
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
+  key_name = "JW-SSH-KEY"
   associate_public_ip_address = true
-
-  subnet_id = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.ssh.id]
+  subnet_id                    = aws_subnet.public.id
+  vpc_security_group_ids       = [aws_security_group.ssh.id,aws_security_group.http.id]
 
   tags = {
-    Name = "Web Server ${count.index}"
+    Name = "Artifactory Server"
   }
 }
+
+resource "aws_instance" "backend" {
+  ami           = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
+  key_name = "JW-SSH-KEY"
+  associate_public_ip_address = true
+  subnet_id                    = aws_subnet.public.id
+  vpc_security_group_ids       = [aws_security_group.ssh.id, aws_security_group.http.id]
+
+  tags = {
+    Name = "Backend Server"
+  }
+}
+
+resource "aws_instance" "frontend" {
+  ami           = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
+  key_name = "JW-SSH-KEY"
+  associate_public_ip_address = true
+  subnet_id                    = aws_subnet.public.id
+  vpc_security_group_ids       = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "Frontend Server"
+  }
+}
+
+
 
 # Create DynamoDB table named workouts and add
 resource "aws_dynamodb_table" "workouts" {
@@ -100,6 +169,14 @@ resource "aws_dynamodb_table" "workouts" {
 }
 
 # Output public IP addresses of the instances
-output "public_ip" {
-  value = aws_instance.webserver[*].public_ip
+output "artifactory_public_ip" {
+  value = aws_instance.artifactory.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
+}
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
 }
